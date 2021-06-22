3403d19696fdcad3959eefb8fcac9e2fcb5dd7e4
Petur Subev <petur.subev@gmail.com>
2021-06-22 19:54:31 +0300

    Updates

--- README.md
+++ README.md
@@ -6,9 +6,5 @@ TODO
 
 - [] Currently works only on localhost
 - [] Untracked files are not handled
-- [] NPX support
 - [] validate that every client is starting from the same hash, otherwise drop connection
-    -awesome
-    -lksdjf
-
-
+- [] NPX support
--- common.ts
+++ common.ts
@@ -1,16 +1,25 @@
+import * as chokidar from "chokidar";
+import * as fs from "fs";
 import { Observable } from "rxjs";
 import * as shell from "shelljs";
-
-import * as chokidar from "chokidar";
-
-// One-liner for current directory
 
 export const localFileChange$ = new Observable<{
   filename: string;
   diff: shell.ShellString;
   stat: shell.ShellString;
 }>((subscriber) => {
-  const watcher = chokidar.watch(".").on("change", (filename) => {
+  const ignored = [
+    ".git",
+    ...(fs.existsSync(".gitignore")
+      ? fs
+          .readFileSync(".gitignore")
+          .toString()
+          .split("\n")
+          .filter((x) => x)
+      : []),
+  ];
+  console.log({ ignored });
+  const watcher = chokidar.watch(".", { ignored }).on("change", (filename) => {
     if (filename) {
       const diff = shell.exec(`git diff ${filename}`);
       const stat = shell.exec(`git diff --stat ${filename}`);
