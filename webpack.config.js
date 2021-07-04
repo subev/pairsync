b95191259927fb01caca437a4f9961ce214bbb84
Petur Subev <petur.subev@gmail.com>
2021-07-04 23:16:19 +0300

    Configure webpack for build

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  mode: "production",
+  entry: {
+    client: "./client.ts",
+    server: "./server.ts",
+  },
+  output: {
+    filename: "[name].js",
+    path: __dirname + "/webpack-output/",
+  },
+  target: "node",
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: [".ts", ".tsx", ".js", ".node"],
+  },
+  module: {
+    rules: [
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      { test: /\.tsx?$/, loader: "ts-loader" },
+      {
+        test: /.node$/,
+        loader: "node-loader",
+      },
+    ],
+  },
+  externals: [nodeExternals()],
+};
